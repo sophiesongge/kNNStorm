268e86ec5eaeafe0ed7f66c33cf8eb639c03604f
sophiesongge <sophiesongge@gmail.com>
2016-02-03 18:27:27 +0100

    kNNSpout done

--- src/storm/spout/kNNSpout.java
+++ src/storm/spout/kNNSpout.java
@@ -1,4 +1,5 @@
 package storm.spout;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,8 +9,8 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.util.Random;
 
+import storm.topology.kNNTopology;
 import kNN.Element;
-import storm.rdf.RDFTriple;
 import backtype.storm.spout.SpoutOutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
@@ -21,19 +22,33 @@ import backtype.storm.utils.Utils;
 public class kNNSpout extends BaseRichSpout{
 	
 	SpoutOutputCollector _collector;
-	Random _rand;
-	private FileReader fileReader;
+	private Random r;
+	//private FileReader fileReader;
 	private static int k;//the number of nearest neighbors
 	private static int d;//dimension
-	static String filePathR; //the file path for data set R
-	static String filePathS; //the file path for data set S
+	//static String filePathR; //the file path for data set R
+	//static String filePathS; //the file path for data set S
+	private int numberOfPartition;
+	//private int recIdOffset;
+	//private int coordOffset;
+	BufferedReader readerR;
+	BufferedReader readerS;
+
 	
+	public kNNSpout(int k, int d, int p){
+		this.k = k;
+		this.d= d;
+		this.numberOfPartition = p;
+		this.r = new Random();
+		r = new Random();
+	}
 	
 	public void open(Map conf, TopologyContext context,
 			SpoutOutputCollector collector) {
 		
 		this._collector = collector;
-		this._rand = new Random();
+		this.readerR = kNNTopology.readerR;
+		this.readerS = kNNTopology.readerS;
 		// TODO Auto-generated method stub
 		
 	}
@@ -41,65 +56,61 @@ public class kNNSpout extends BaseRichSpout{
 	public void nextTuple() {
 		Utils.sleep(100);
 		generateTuple();
-		
 	}
+	
 	
 	public void generateTuple(){
 		
-		File file = new File(filePathR);
-		BufferedReader reader = null;
-		ArrayList<Element> readList = new ArrayList<Element>();
 		try{
-			reader = new BufferedReader(new FileReader(file));
-			String tempsString = null;
-			while((tempsString = reader.readLine())!=null){
-				String parts[] = tempsString.split(" +");
-				int id= Integer.parseInt(parts[0]);
-				/*System.out.print("id: "+parts[0]);
-				System.out.print(" x: "+parts[0+1]);
-				System.out.println(" y: "+parts[1+1]);*/				
+			String tempsStringR = null;
+			while((tempsStringR = readerR.readLine())!=null){
+				String parts[] = tempsStringR.split(" +");
+				int id = Integer.parseInt(parts[0]);
 				float[] coord = new float[d];
 				for(int ii=0; ii<d; ii++){
 					try{
 						coord[ii] = Float.valueOf(parts[1+ii]);
 					}catch(NumberFormatException ex){
-						
+						//Do Nothing
 					}
 				}
-				Element le = new Element(id, coord);
-				le.setId(id);
-				le.setCoord(coord);
-				readList.add(le);
-			}
-		}catch(IOException e){
-			e.printStackTrace();
-		}finally{
-			if(reader != null){
-				try{
-					reader.close();
-				}catch(IOException e1){
-					//Do nothing
+				
+				Element er = new Element(id, coord);
+				er.setId(id);
+				er.setCoord(coord);
+				
+				int partId = r.nextInt(numberOfPartition);
+				int groupId = 0;
+				
+				for(int i=0; i<numberOfPartition; i++){
+					groupId = partId * numberOfPartition + i;
+					_collector.emit(new Values(er, groupId));
 				}
 			}
-		}
-		
-		
-		try{
-			String tempsString = null;
-			while((tempsString = _reader.readLine())!=null){
-				String parts[] = tempsString.split(" +");
-				String Subject = parts[0];				
-				String Predicate = parts[1];
-				String Object = parts[2];
+			String tempsStringS = null;
+			while((tempsStringS = readerS.readLine())!=null){
+				String parts[] = tempsStringS.split(" +");
+				int id = Integer.parseInt(parts[0]);
+				float[] coord = new float[d];
+				for(int ii=0; ii<d; ii++){
+					try{
+						coord[ii] = Float.valueOf(parts[1+ii]);
+					}catch(NumberFormatException ex){
+						//Do Nothing
+					}
+				}
 				
-				RDFTriple rdf = new RDFTriple(Subject, Predicate, Object);
-				rdf.setSubject(Subject);
-				rdf.setPredicate(Predicate);
-				rdf.setObject(Object);
+				Element er = new Element(id, coord);
+				er.setId(id);
+				er.setCoord(coord);
 				
-				//emit every line, Values is an instance of ArrayList
-				_collector.emit(new Values(Subject, Predicate, Object));
+				int partId = r.nextInt(numberOfPartition);
+				int groupId = 0;
 				
+				for(int i=0; i<numberOfPartition; i++){
+					groupId = partId + numberOfPartition * i;
+					_collector.emit(new Values(er, groupId));
+				}
 			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
--- src/storm/topology/kNNTopology.java
+++ src/storm/topology/kNNTopology.java
@@ -1,6 +1,78 @@
 package storm.topology;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import storm.spout.kNNSpout;
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+
 public class kNNTopology{
+	public static BufferedReader readerR;
+	public static BufferedReader readerS;
+	
+	public static void main(String[] args) throws Exception{
+		String filePathR = "./data/databig1.txt";
+		String filePathS = "./data/databig2.txt";
+		File fileR = new File(filePathR);
+		File fileS = new File(filePathS);
+		readerR = null;
+		readerS = null;
+		try{
+			readerR = new BufferedReader(new FileReader(fileR));
+			readerS = new BufferedReader(new FileReader(fileS));
+			stormCall();
+				
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			if(readerR != null){
+				try{
+					readerR.close();
+				}catch(IOException e1){
+					//Do nothing
+				}
+			}
+			if(readerS != null){
+				try{
+					readerS.close();
+				}catch(IOException e2){
+					//Do nothing
+				}
+				}
+			}
+		}
 	
 	
+	public static void stormCall() throws InterruptedException
+	{
+		Config config = new Config();
+		config.setDebug(true);
+		
+		TopologyBuilder builder = new TopologyBuilder();
+		
+		builder.setSpout("spout_getdata", new kNNSpout(3, 2, 3),2);
+
+		builder.setBolt("bolt_bloomfilter", new BoltCreatBF(),3).fieldsGrouping("spout_getdata", new Fields("Predicate"));
+
+		builder.setBolt("bolt_test", new BoltTest(),1).shuffleGrouping("spout_getdata");
+
+		LocalCluster cluster = new LocalCluster();
+		cluster.submitTopology("kNNStorm", config, builder.createTopology());
+		Thread.sleep(10000);
+		
+		//Sander: cluster shutdown throws IOException, but adding try/catch states that it is an Unreachable catch block for IOException.
+		try{
+			cluster.shutdown();	
+			throw new IOException("test");//Used as debug, otherwise we got the error saying this block couldn't generate an IOException
+		} catch(IOException e){
+			System.out.println("IOException when shutting down the cluster, continued afterwards, error message: " + e.getMessage());
+		}
+	}
 }
